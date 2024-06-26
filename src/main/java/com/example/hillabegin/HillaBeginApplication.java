809c3593ae229415d0d4048d1ddfb3d46c6c445b
Md. Alif Babu <muhammad.alifbabu@gmail.com>
2024-06-26 18:44:41 +0600

    hilla begin

---
+++ src/main/java/com/example/hillabegin/HillaBeginApplication.java
@@ -0,0 +1,13 @@
+package com.example.hillabegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillaBeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HillaBeginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hillabegin/HillaBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hillabegin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HillaBeginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
